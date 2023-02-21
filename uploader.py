57ff56f7026492f833692367b72cfccbe813367c
sunni426 <sunni426@bu.edu>
2023-02-21 15:17:45 -0500

    file structure setup

--- uploader.py
+++ uploader.py
@@ -1,24 +1,8 @@
 '''
 
 
-attach(filepath, filetype)
-    Returns 0 if successful
-upload(file, filetype)
-    Returns fileID if successful
-login(Username, password)
-    Returns 0 if successful
-
-
-
-def attach(filepath, filetype):
-    selectfile(filepath)
-
-    attachfile
-
-
-
-def upload(file)
-
-
 
 '''
+
+
+import os.path as path
